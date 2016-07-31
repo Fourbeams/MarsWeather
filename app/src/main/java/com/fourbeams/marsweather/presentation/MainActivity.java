492cccd2435feb0107ec516cb55b3e4dae8f64fa
creatiwww <creatiwww@yandex.ru>
2016-08-01 00:59:08 +0300

    status bar color changed

--- app/src/main/java/com/fourbeams/marsweather/presentation/MainActivity.java
+++ app/src/main/java/com/fourbeams/marsweather/presentation/MainActivity.java
@@ -4,18 +4,15 @@ import android.app.LoaderManager;
 import android.content.*;
 import android.database.ContentObserver;
 import android.database.Cursor;
-import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
-import android.widget.VideoView;
 import com.fourbeams.marsweather.R;
 import com.fourbeams.marsweather.domain.Processor;
 import com.fourbeams.marsweather.persistence.MarsWeatherContentProvider;
